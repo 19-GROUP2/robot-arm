587481f57869548ba6ed6955b6193e34b62075b5
KCSAbeywickrama <abeywickramakcs.19@uom.lk>
2023-11-29 21:22:27 +0530

    implement updateServos()

--- arduino/ArmController/ArmController.ino
+++ arduino/ArmController/ArmController.ino
@@ -3,8 +3,10 @@
 String inputString = "";    // a String to hold incoming data
 bool inputComplete = false; // whether the string is complete
 
-const int SERVO_SPEED_MAX = 40;                      // maximum speed of the servos
-const int SERVO_COUNT = 5;                           // maximum number of angles to load
+const int SERVO_SPEED_MAX = 40; // maximum speed of the servos
+const int SERVO_COUNT = 5;
+const int JOINT_COUNT = SERVO_COUNT - 1;
+const int GRIPPER_INDEX = SERVO_COUNT - 1;           // maximum number of angles to load
 const int servoPins[SERVO_COUNT] = {3, 5, 6, 9, 11}; // pins for the servos
 VarSpeedServo servos[SERVO_COUNT];                   // array of VarSpeedServo objects
 int angles[SERVO_COUNT];                             // array to store the loaded angles
@@ -25,47 +27,55 @@ void loop()
 {
   if (inputComplete)
   {
-    int startIndex = 0;
-    int endIndex = inputString.indexOf(sepChar);
-    for (int i = 0; i < SERVO_COUNT; i++)
-    {
-      if (endIndex == -1)
-      {
-        angles[i] = inputString.substring(startIndex).toInt();
-        break;
-      }
-      else
-      {
-        angles[i] = inputString.substring(startIndex, endIndex).toInt();
-        startIndex = endIndex + 1;
-        endIndex = inputString.indexOf(sepChar, startIndex);
-      }
-    }
-
-    for (int i = 0; i < SERVO_COUNT; i++)
-    {
-
-      Serial.print(angles[i]);
-      Serial.print(' ');
-      servos[i].write(angles[i], SERVO_SPEED_MAX, false);
-    }
-    Serial.println();
-
-    bool wait = true;
-    while (wait)
-    {
-      wait = false;
-      for (int i = 0; i < SERVO_COUNT; i++)
-      {
-        if (servos[i].read() != angles[i])        
-          wait = true;  
-      }
-      delay(50);
-    }
+    for (int i = 0; i < 1; i++)
+      updateServos();
 
     inputString = "";
     inputComplete = false;
   }
+}
+
+void updateServos()
+{
+  int startIndex = 0;
+  int endIndex = inputString.indexOf(sepChar);
+  for (int i = 0; i < SERVO_COUNT; i++)
+  {
+    if (endIndex == -1)
+    {
+      angles[i] = inputString.substring(startIndex).toInt();
+      break;
+    }
+    else
+    {
+      angles[i] = inputString.substring(startIndex, endIndex).toInt();
+      startIndex = endIndex + 1;
+      endIndex = inputString.indexOf(sepChar, startIndex);
+    }
+  }
+
+  for (int i = 0; i < JOINT_COUNT; i++)
+  {
+    Serial.print(angles[i]);
+    Serial.print(' ');
+    servos[i].write(angles[i], SERVO_SPEED_MAX, false);
+  }
+
+  Serial.println();
+
+  bool wait = true;
+  while (wait)
+  {
+    wait = false;
+    for (int i = 0; i < JOINT_COUNT; i++)
+    {
+      if (servos[i].read() != angles[i])
+        wait = true;
+    }
+    delay(50);
+  }
+
+  servos[GRIPPER_INDEX].write(angles[GRIPPER_INDEX], SERVO_SPEED_MAX, true);
 }
 
 void serialEvent()

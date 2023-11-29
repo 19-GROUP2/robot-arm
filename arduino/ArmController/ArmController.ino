2aac44236a501e350737ae72af4593697f861e73
KCSAbeywickrama <abeywickramakcs.19@uom.lk>
2023-11-29 14:51:51 +0530

    implement arm controller

--- arduino/ArmController/ArmController.ino
+++ arduino/ArmController/ArmController.ino
@@ -3,9 +3,9 @@
 String inputString = "";    // a String to hold incoming data
 bool inputComplete = false; // whether the string is complete
 
-const int SERVO_SPEED_MAX = 50; // maximum speed of the servos
-const int SERVO_COUNT = 4; // maximum number of angles to load
-const int servoPins[SERVO_COUNT] = {9, 10, 11, 12}; // pins for the servos
+const int SERVO_SPEED_MAX = 30; // maximum speed of the servos
+const int SERVO_COUNT = 5; // maximum number of angles to load
+const int servoPins[SERVO_COUNT] = {3, 5, 6, 9, 11}; // pins for the servos
 VarSpeedServo servos[SERVO_COUNT]; // array of VarSpeedServo objects
 int angles[SERVO_COUNT];   // array to store the loaded angles
 char sepChar = ' ';        // character used to separate the angles
@@ -19,7 +19,7 @@ void setup()
   for (int i = 0; i < SERVO_COUNT; i++)
   {
     servos[i].attach(servoPins[i]);
-    servos[i].write(0, SERVO_SPEED_MAX, false);
+    //servos[i].write(0, SERVO_SPEED_MAX, false);
   }
 }
 
@@ -46,12 +46,12 @@ void loop()
 
     for (int i = 0; i < SERVO_COUNT; i++)
     {
-      Serial.print("angle");
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.println(angles[i]);
-      servos[i].write(angles[i], SERVO_SPEED_MAX, false);
+      
+      Serial.print(angles[i]);
+      Serial.print(' ');
+      servos[i].write(angles[i], SERVO_SPEED_MAX, true);
     }
+    Serial.println();
 
     inputString = "";
     inputComplete = false;
